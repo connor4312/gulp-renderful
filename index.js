5410174d1996c8d41705af7852696391fce0c52a
Connor Peet <connor@connorpeet.com>
2014-07-18 09:51:39 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,94 @@
+var gutil   = require('gulp-util'),
+    express = require('express'),
+    path    = require('path'),
+    through = require('through'),
+    fs      = require('fs-extra'),
+    async   = require('async'),
+    app     = express(),
+    File    = gutil.File,
+    PluginError = gutil.PluginError;
+
+module.exports = function(fileName, opt) {
+
+    opt            = opt            || {};
+    opt.engine     = opt.engine     || require('ejs').renderFile;
+    opt.extensions = opt.extensions || [];
+    opt.data       = opt.data       || {};
+
+    var self     = this,
+        files    = [],
+        filePath = path.join(path.resolve(__dirname), '.tmp'),
+        exts     = [];
+
+    app.engine('ejs', opt.engine);
+    app.set('view engine', 'ejs');
+    app.set('views', filePath);
+
+    function registerExtension(ext) {
+        app.engine(ext.substring(1), opt.engine);
+        exts.push(ext);
+    }
+
+    for (var i = 0, l = opt.extensions.length; i < l; i++) {
+        registerExtension(opt.extensions[i]);
+    }
+
+    function store(file) {
+        if (file.isNull()) {
+            return;
+        }
+
+        if (file.isStream()) {
+            return this.emit('error', new PluginError('gulp-renderful',  'Streaming not supported'));
+        }
+
+        var p      = path.join(filePath, file.path),
+            ext    = path.extname(file.path);
+
+        fs.mkdirs(path.dirname(p), function () {
+            file.pipe(fs.createWriteStream(p));
+        });
+
+        if (!opt.extensions.length || exts.indexOf(ext) !== -1) {
+            registerExtension(ext);
+        }
+
+        files.push(file);
+    }
+
+    function render() {
+
+        var self = this;
+
+        async.each(files, function (file, callback) {
+
+            var ext = path.extname(file.path);
+
+            if (opt.extensions.length && opt.extensions.indexOf(ext) === -1) {
+                self.emit(file);
+                callback();
+                return;
+            }
+
+            app.render(file.path, opt.data, function (err, html) {
+                if (err) {
+                    return callback(err);
+                }
+
+                file.contents = new Buffer(html);
+                self.push(file);
+
+                callback();
+            });
+
+        }, function (err) {
+            if (err) {
+                self.emit('error', new PluginError('gulp-renderful',  err));
+            }
+
+            fs.remove(filePath);
+        });
+    }
+
+    return through(store, render);
+};
---
+++ readme.md
@@ -0,0 +1,36 @@
+# gulp-renderful
+
+This package is designed to make the building of static sites easier and more enjoyable. Rather than using ugly PHP `includes`, you can now use EJS templating (or whatever else you like) backed by Express to render HTML. This allows you to, for example, include view partials with `<%- include somepartial.ejs %>`.
+
+## Example
+
+```js
+gulp.task('html', function() {
+    gulp.src('src/*.html')
+        .pipe(require('../gulp-renderful')())
+        .pipe(gulp.dest('./dist'));
+});
+```
+
+## Options: renderful([options])
+
+### engine
+
+Type: `object` Default: `require('ejs').renderFile`
+
+Allows you to pass your own rendering engine in, to use instead of EJS.
+
+### extensions
+
+Type: `array` Default: `[]`
+
+When a non-empty array, only files with these extensions will be processed. For more capabilities (e.g. globbing), you can take a look at [gulp-if](https://github.com/robrich/gulp-if).
+
+### data
+
+Type: `object` Default: `{}`
+
+Data to be passed into the views.
+
+
+
